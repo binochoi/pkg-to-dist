43f2c6860480647bebeed2dcd2c9d14e2e6abdd1
bino <bonoself@gmail.com>
2024-09-05 03:00:49 +0900

    refactor:

--- src/utils/createFolderIfNotExist.ts
+++ src/utils/createFolderIfNotExist.ts
@@ -2,12 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export default (folderName: string) => {
-    // dist 폴더 경로 지정
     const distFolderPath = path.join(__dirname, folderName);
-
-    // dist 폴더가 존재하는지 확인
     if (!fs.existsSync(distFolderPath)) {
-        // 폴더가 없으면 생성
-        fs.mkdirSync(distFolderPath, { recursive: true });
+        fs.mkdirSync(distFolderPath);
     }
 }
