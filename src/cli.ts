3cbe5692c692124e9cd0d1fe4a6f06a134e5a5eb
bino <bonoself@gmail.com>
2024-09-05 02:52:36 +0900

    feat: create dist folder if not exist

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { replaceObjValRecursively } from './utils/replaceObjValRecursively';
+import createFolderIfNotExist from './utils/createFolderIfNotExist';
 
 const packageJsonText = fs.readFileSync('package.json', { encoding: 'utf-8' });
 const packageJson = JSON.parse(packageJsonText);
@@ -9,4 +10,5 @@ const newPackageJson = {
     bin: replaceObjValRecursively(packageJson.bin, './dist/', './'),
     exports: replaceObjValRecursively(packageJson.exports, './dist/', './')
 }
+createFolderIfNotExist('dist');
 fs.writeFileSync('dist/package.json', JSON.stringify(newPackageJson));
---
+++ src/utils/createFolderIfNotExist.ts
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+export default (folderName: string) => {
+    // dist 폴더 경로 지정
+    const distFolderPath = path.join(__dirname, folderName);
+
+    // dist 폴더가 존재하는지 확인
+    if (!fs.existsSync(distFolderPath)) {
+        // 폴더가 없으면 생성
+        fs.mkdirSync(distFolderPath, { recursive: true });
+    }
+}
