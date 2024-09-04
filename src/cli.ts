777872ad519747b8de0f8fc92eba720522e11d3e
bino <bonoself@gmail.com>
2024-09-05 03:04:53 +0900

    add main, module, type props

--- src/cli.ts
+++ src/cli.ts
@@ -6,9 +6,12 @@ const packageJsonText = fs.readFileSync('package.json', { encoding: 'utf-8' });
 const packageJson = JSON.parse(packageJsonText);
 const newPackageJson = {
     ...packageJson,
+    exports: replaceObjValRecursively(packageJson.exports, './dist/', './'),
     files: ['*'],
-    bin: replaceObjValRecursively(packageJson.bin, './dist/', './'),
-    exports: replaceObjValRecursively(packageJson.exports, './dist/', './')
+    main: packageJson.main?.replace(new RegExp('./dist/', 'g'), './'),
+    module: packageJson.main?.replace(new RegExp('./dist/', 'g'), './'),
+    type: packageJson.main?.replace(new RegExp('./dist/', 'g'), './'),
+    bin: packageJson.bin && replaceObjValRecursively(packageJson.bin, './dist/', './'),
 }
 createFolderIfNotExist('dist');
 fs.writeFileSync('dist/package.json', JSON.stringify(newPackageJson));
